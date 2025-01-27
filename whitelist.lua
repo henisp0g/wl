bc16c2df08098e288ced5c5b8ae0bd1e23ecc8c8
henisp0g <henispogs@gmail.com>
2025-01-27 15:18:06 +0000

    Update and rename whitelist.json to whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,13 @@
+return {
+    ["hashfunctionn"] = {
+        icon = 18486110781,
+        color = nil,  -- No color
+        chaticon = true
+    },
+    ["anotherUser"] = {
+        icon = 0,
+        color = Color3.fromRGB(0, 255, 0),  -- Green color
+        chaticon = false
+    },
+    -- Add more users here
+}
